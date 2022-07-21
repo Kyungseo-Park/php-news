4873c91c7e9061b80b5e9a9beea399ca8f56184a
Kyungseo-Park <troublesome.dev@gmail.com>
2022-07-21 21:05:40 +0900

    fix: 조건절 반대로해서 not found 이슈 있어서 수정

--- pages/[category]/[postId].tsx
+++ pages/[category]/[postId].tsx
@@ -1,18 +1,12 @@
 import axios from "axios";
-import { GetServerSideProps, GetStaticProps, GetStaticPropsContext } from "next";
+import { GetStaticProps, GetStaticPropsContext } from "next";
 import Layout from "@components/Layouts";
-import withGetServerSideProps from "@lib/utils/withServerSideProps";
-import { serialize } from "next-mdx-remote/serialize";
-
-import fs from "fs";
-import path from "path";
 import { isCategory } from "@lib/utils/isCategory";
 import { API, Article } from "types/article";
 import ArticleDetail from "@components/ArticleDetail";
-import markdownToHtml, { getDocByUrl } from "@lib/utils/getDocs";
 
-const Page = ({ article, doc, content }: { article: Article; doc: any; content: any }) => {
-  console.log(article, doc, content);
+const Page = ({ article, content }: { article: Article; content: any }) => {
+  console.log(article, content);
 
   return (
     <Layout
@@ -28,12 +22,8 @@ const Page = ({ article, doc, content }: { article: Article; doc: any; content: 
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
-  console.log(params);
-
   const postId = params?.postId;
   const category = params?.category?.toString();
-
-  console.log(postId, category);
 
   if (!isCategory(category) || !postId) {
     return {
@@ -44,15 +34,12 @@ export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsC
 
   try {
     const { data } = await axios.get<API.GET.Articles.ID>(paths);
-    const transtaionUrl = data.data.translated_url;
-    const doc = getDocByUrl(transtaionUrl);
-    const content = await markdownToHtml(doc.content || "");
+    const content = data.data.translated_content; // await markdownToHtml(doc.content || "");
 
-    if (!content) {
+    if (content) {
       return {
         props: {
           article: data.data,
-          doc,
           content,
         },
       };
